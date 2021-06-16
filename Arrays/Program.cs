2e2413e4f5e605c95dc079db4da82fbc32ea7849
Andrea-Wafula <86024138+Andrea-Wafula@users.noreply.github.com>
2021-06-16 15:10:14 -0700

    Add files via upload

---
+++ Arrays/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] world = new string[4];
+            world[0] = "Mercury";
+            world[1] = "Venus";
+            world[2] = "Earth";
+            world[3] = "Mars";
+
+            Console.WriteLine(world[0]);
+
+
+
+            int[] numbers = new int[3];
+            numbers[0] = 1;
+
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
+
+
+
+            bool[] flags = new bool[3];
+            flags[0] = true;
+
+            Console.WriteLine(flags[0]);
+            Console.WriteLine(flags[1]);
+            Console.WriteLine(flags[2]);
+
+
+            string[] names = new string[3] {"Jack","John","Mary"};
+        }
+        
+    }
+}
