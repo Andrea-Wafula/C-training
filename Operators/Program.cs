4388ae766f144a7eecfdf1daa51789504df9dd3b
Andrea-Wafula <86024138+Andrea-Wafula@users.noreply.github.com>
2021-06-16 15:05:08 -0700

    Add files via upload
    This folder shows the use of various Operators in C# Fundamentals. They can be categorised under
    1. Arithmetic Operators; include Addition + ,Subtraction -, Multiplication *, Division /, Remainder %
    2.  Comparison Operators; i.e. Greater Than >, Less than <, Equal To == and Not Equal !=
    3. Assignment Operators.
    4. Logical Operators.
    5. Bitwise Operators; And & and Or /

---
+++ Operators/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Arithmentic Operators include Addition + ,Subtraction -, Multiplication *, Division /, Remainder %
+
+            //Types of Increments
+            //Prefix Increments 
+            int a = 1;
+            int b = ++a;
+            Console.WriteLine(b);
+
+            //Postfix Increments
+            int c = 1;
+            int d = c++;
+            Console.WriteLine(d);
+
+            // Comparison Operators include Equal to ==, Not Equal to !==, Greater Than >, Less Than <, Greater Than or Equal To >=,
+            // Less Than or Equal To <=.
+
+            // Assignment Operators include Assignment =, Additional Assignment +=, Subtraction Assignment -=, Division Assignment /=,
+            // Multiplication Assignment *=
+
+            int e = 1;
+            int f = e += 3;
+            int g = e -= 3;
+            int h = e *= 3;
+            int i = e /= 3;
+
+            Console.WriteLine(e);
+            Console.WriteLine(f);
+            Console.WriteLine(g);
+            Console.WriteLine(h);
+            Console.WriteLine(i);
+
+            // Logical Operators include And &&, Or ||, Not !
+
+            Console.WriteLine(a > c && c > e);
+            Console.WriteLine(!(a > c || c > e));
+            Console.WriteLine(a > c || c == a);
+        
+
+            // Bitwise Operators include And &, Or |
+
+
+
+        }
+    }
+}
