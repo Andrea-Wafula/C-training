b02a383128333e8f7a20bf07c3bc81310cafaeda
Andrea-Wafula <86024138+Andrea-Wafula@users.noreply.github.com>
2021-06-16 15:14:42 -0700

    Add files via upload
    Enums are examples of special classes.
    
     An enum is a special "class" that represents a group of constants (unchangeable variables, like final variables).
    
    To create an enum, use the enum keyword (instead of class or interface), and separate the constants with a comma. Note that they should be in uppercase letters:
    
    ~From www.W3schools.com

---
+++ Enums/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Enums
+{
+
+    public enum ShippingMethods
+    {
+        RegularAirMail = 1,
+        RegisteredAirMail = 2,
+        Express = 3
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Converting Enum (ShippingMethods.Express) into an int
+            var method = ShippingMethods.Express;
+            Console.WriteLine((int)method);
+
+            //Creating (methodId) & converting into enum ShippingMethods
+            var methodId = 3;
+            Console.WriteLine((ShippingMethods)methodId);
+
+            //Converting method (ShippingMethods.Express) into a string
+            Console.WriteLine(method);
+            Console.WriteLine(method.ToString());
+
+            var methodName = "Express";
+            var shippingMethod = (ShippingMethods) Enum.Parse(typeof(ShippingMethods), methodName);
+        }
+    }
+}
