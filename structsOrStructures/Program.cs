a4b76cff8221c08a5f28807e0aed3f18cc411afb
Andrea-Wafula <86024138+Andrea-Wafula@users.noreply.github.com>
2021-06-16 15:17:37 -0700

    Add files via upload

---
+++ structsOrStructures/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace structsOrStructures
+{
+    public class Program
+    {
+        public struct RgbColors
+        {
+            public int Red;
+            public int Green;
+            public int Blue;
+        }
+
+        public struct CompassPoints
+        {
+            public int North;
+            public int East;
+            public int South;
+            public int West;
+        }
+
+        public struct VsCode
+        {
+            public int CSharp;
+            public int FSharp;
+            public int VisualBasic;
+        }
+
+        public struct ArchOrganisations
+        {
+            public int Grid;
+            public int Radial;
+            public int Centralised;
+            public int Linear;
+            public int Spatial;
+
+        }
+    }
+}
