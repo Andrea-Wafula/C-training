723c64e017430e54e79c112ea4b3274a57e8d68f
Andrea-Wafula <86024138+Andrea-Wafula@users.noreply.github.com>
2021-06-29 18:22:17 -0500

    Add files via upload

---
+++ Path/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace Path
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"D:\MyRepos\CodeWithMoshTraining\CSharpTraining\HelloWorld\HelloWorld.sln";
+
+            var dotIndex = path.IndexOf(".");
+            var extension = path.Substring(dotIndex);
+
+
+            Console.WriteLine("Extension: " + Path.GetExtension(path));
+            Console.WriteLine("File name: " + Path.GetFileName(path));
+            Console.WriteLine("File name without extension: " + Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("Directory Name: " Path.GetDirectoryName(path)); ;
+        }
+    }
+}
