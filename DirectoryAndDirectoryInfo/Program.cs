9ce9c12a5b6e563c10ae1e491f596e366ca5db16
Andrea-Wafula <86024138+Andrea-Wafula@users.noreply.github.com>
2021-06-29 18:18:03 -0500

    Add files via upload

---
+++ DirectoryAndDirectoryInfo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace DirectoryAndDirectoryInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //First Example
+            Directory.CreateDirectory(@"c:\temp\folder1");
+            var files = Directory.GetFiles(@"D:\MyRepos\CodeWithMoshTraining\CSharpTraining", "*.sln", SearchOption.AllDirectories);
+            foreach (var file in files)
+            {
+                Console.WriteLine(file);
+            }
+
+            //Second Example
+            var directories = Directory.GetDirectories(@$"D:\MyRepos\CodeWithMoshTraining\CSharpTraining\WorkingWthFiles", "*.*", 
+                SearchOption.AllDirectories);
+            foreach (var directory in directories)
+            {
+                Console.WriteLine(directory);
+            }
+
+            Directory.Exists("...");
+
+            var directoryInfo = new DirectoryInfo("....");
+            directoryInfo.GetFiles();
+            directoryInfo.GetDirectories();
+        }
+    }
+}
